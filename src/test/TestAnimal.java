566db36ff732758eba29bc1568d8638f19a1afaf
Stickki <106401702+Stickki@users.noreply.github.com>
2023-02-20 21:06:35 +0100

    Add files via upload

--- src/test/TestAnimal.java
+++ src/test/TestAnimal.java
@@ -1,15 +1,60 @@
-package test;
+// Родительский класс "Животное"
+class Animal {
+    private String name;
+    private int age;
 
-import domain.Cat;
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void eat() {
+        System.out.println(name + " eating");
+    }
+
+    public void sleep() {
+        System.out.println(name + " sleep");
+    }
+}
+
+// Дочерний класс "Собака"
+class Dog extends Animal {
+    private String breed;
+
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public static void bark(Dog dog) {
+        System.out.println(dog.getName() + " barks");
+    }
+}
+
+// Пример использования классов
 public class TestAnimal {
-
     public static void main(String[] args) {
-        Cat cat = new Cat("Thomas");
-        System.out.println(cat);
-        cat.hunt();
-        cat.eat();
-        cat.speak();
-        cat.play();
+        Animal animal = new Animal("Animal", 5);
+        animal.eat();
+        animal.sleep();
+
+        Dog dog = new Dog("Patron", 3, "Sheepdog");
+        dog.eat();
+        dog.sleep();
+        Dog.bark(dog);
+
+        System.out.println(dog.getName() + " - " + dog.getAge() + " years, breed: " + dog.getBreed());
     }
 }
